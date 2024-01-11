5aacf5c96c05931bfa357c4fdac7885da42feed8
arvidj <arvidj89@gmail.com>
2024-01-11 15:07:16 +0100

    updated uptime monitor

--- examples/monitor_uptime.py
+++ examples/monitor_uptime.py
@@ -1,40 +1,75 @@
+import sys
 import time
-from ping3 import ping, verbose_ping
+import threading
+from ping3 import ping
 from rich.console import Console
 from rich.table import Table
 
 # List of targets
-targets = ['nrk.no', 'vg.no','192.168.0.100']
+targets = ['nrk.no', 'vg.no']
+targets.extend([f'login.p{i}.worldoftanks.eu' for i in range(1,4)])
 
 # Initialize console
 console = Console()
 
 # Function to ping and return status
-def ping_and_return_status(target):
+def ping_and_return_status(target, results):
     try:
         delay = ping(target)
         if delay is None:
-            return 'red', 'No response'
+            results[target] = ('red', 'No response')
         else:
-            return 'green', f'Responded in {delay} seconds'
+            delay = delay * 1000
+            delay = '{:.3f}'.format(delay)
+            # delay = round(delay, 3)  # Round delay to 3 decimal places
+            
+            results[target] = ('green', f'{delay} ms')
     except Exception as e:
-        return 'red', str(e)
+        results[target] = ('red', str(e))
 
 # Function to create and display table
 def display_table():
-    table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("Target", style="dim", width=14)
-    table.add_column("Status", justify="right")
-    table.add_column("Response", justify="right")
+    # Dictionary to store the results
+    results = {}
+    # List to store the threads
+    threads = []
 
+    # Create and start a new thread for each target
     for target in targets:
-        color, response = ping_and_return_status(target)
+        thread = threading.Thread(target=ping_and_return_status, args=(target, results))
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+
+    # Create the table
+    USE_EMOJIS = False
+    if USE_EMOJIS:
+        target_str = "\U0001F5A5"
+        up_str = "Status"
+        response_str = "\U0001F553"
+    else:
+        target_str = "Target"
+        up_str = "Up"
+        response_str = "Response"
+
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column(target_str, style="dim", width=24)
+    table.add_column(up_str, justify="center", width=2)
+    table.add_column(response_str, justify="right")
+
+    # Add a row to the table for each target
+    for target in targets:
+        color, response = results[target]
         table.add_row(target, f'[bold {color}]●[/bold {color}]', response)
 
+    # Clear the console and print the table
     console.clear()
     console.print(table)
 
 # Continuously monitor
 while True:
     display_table()
-    time.sleep(10)
+    time.sleep(1)
