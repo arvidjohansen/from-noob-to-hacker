74899c078f969e33848d9f7e8cf90cd3f7a6b14c
arvidj <arvidj89@gmail.com>
2024-01-23 11:00:11 +0100

    upgraded tank game tho it still sawhkhs

--- games/world_of_tanks/wotgame.py
+++ games/world_of_tanks/wotgame.py
@@ -20,19 +20,22 @@ tank_pos = [WIDTH // 2, HEIGHT // 2]
 class Bullet:
     def __init__(self, pos):
         self.pos = list(pos)
-        self.speed = 1
+        self.speed = 2
         self.state = 'flying'
         self.image = pygame.image.load('bullet.png')
         self.explosion_image = pygame.image.load('explosion.png')
         print('created bullet')
 
     def move(self):
-        self.pos[1] -= self.speed
+        self.pos[0] += self.speed
         print(f'moved bullet to: {self.pos}')
 
     def check_collision(self, target):
-        # Add your collision detection logic here
-        pass
+        bullet_rect = pygame.Rect(self.pos[0], self.pos[1], self.image.get_width(), self.image.get_height())
+        target_rect = pygame.Rect(target[0], target[1], tank.get_width(), tank.get_height())
+    
+        if bullet_rect.colliderect(target_rect):
+            self.explode()
 
     def explode(self):
         print(f'bullet exploded at {self.pos}')
@@ -61,9 +64,9 @@ while True:
         tank_pos[1] += SPEED
     
     if keys[pygame.K_SPACE]:
-        bullets.append(Bullet(tank_pos))
-
-    
+        # Create the bullet at the top of the tank
+        bullet_pos = [tank_pos[0] + tank.get_width() , tank_pos[1]]
+        bullets.append(Bullet(bullet_pos))
 
 
     # Draw everything
